9ca9e23a2e100ccb91dd87c73a3ef7dd427a8aad
Guofu Li <48232336+cybercampus@users.noreply.github.com>
2023-04-16 19:10:01 +0800

    Add files via upload

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # polls的首页
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # 显示第三个问题的内容，例如: /polls/2/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # 显示第三个问题的内容回答，例如:: /polls/2/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # 显示第三个问题的打分，例如:: /polls/2/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,25 @@ from django.shortcuts import get_object_or_404, render
 from .models import Question, Choice
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        #返回最后发布的5个问题
+        #return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.ListView):
+    model = Question
+    template_name = 'detail.html'
+
+class ResultsView(generic.ListView):
+    model = Question
+    template_name = 'result.html'
 
 '''
 #定义视图函数
@@ -19,7 +38,7 @@ def index(request):
 def index(request):
     context = {'title':'我的站点' ,'first_name': '乙丙', 'last_name': '白'}
     return render(request, 'index.html', context)
-'''
+
 #从数据库中读取数据，传递给模板
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -27,11 +46,11 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return render(request, 'index.html', context)
-'''
+
 #根据 id 返回一个问题的详细信息
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
-'''
+
 #从数据库中读取数据，传递给模板
 def detail(request, question_id):
     try:
@@ -39,11 +58,15 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'detail.html', {'question': question})
-'''
+
 #根据问题 id 返回问题的回答
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'result.html', {'question': question})
 
 #根据问题题 id 返回问题的打分
 def vote(request, question_id):
@@ -65,6 +88,3 @@ def vote(request, question_id):
         # 在成功处理 POST 数据后，返回一个 HttpResponseRedirect 刷新页面。
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'result.html', {'question': question})
